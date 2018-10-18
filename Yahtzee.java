f4ed60d9a9720884d5ab813ddf8a025a6cfe2c51
Luukvv87 <luukvanvorselen@gmail.com>
2018-10-18 14:21:49 +0200

    Tweede versie 3 worpen

--- Yahtzee.java
+++ Yahtzee.java
@@ -13,14 +13,14 @@ x 3. Maak een losse methode werpen() in het YahtzeeSPel. Deze methode heeft int 
   	 Zorg dat de aanroep van deze methode er voor zorgt dat er een getal tussen 1 en 6 terug komt.
 
  Echte dobbelstenen:
- 4. Maak de methode een onderdeel van de klasse Dobbelsteen. Maak in de klasse YahtzeeSpel een Arraylist van Dobbelstenen.
- 	In de constructor van het YahtzeeSpel voeg je 5 dobbelstenen toe aan deze ArrayList.
- 	In de loop van spelen wordt over de Arraylist heengegaan met een enhanced for-loop en werp je iedere dobbelsteen.
- 	Hou in je dobbelsteen het resultaat van de worp vast.
- 5. Maak een losse methode vasthouden(). Via een Scanner vraagt het programma om invoer.
- 	De invoer wordt via Integer.parseInt() omgezet tot een blokkeerarray van 0 en 1 bijvoorbeeld 11001.
- 	Dit blokkeerarray is een field van het YahtzeeSpel.
- 6. Loop in de tweede worp over de arraylist heen en alleen als in de blokkeerarray een 0 staat wordt de dobbelsteen opnieuw geworpen. 
+x 4. Maak de methode een onderdeel van de klasse Dobbelsteen. Maak in de klasse YahtzeeSpel een Arraylist van Dobbelstenen.
+ 	 In de constructor van het YahtzeeSpel voeg je 5 dobbelstenen toe aan deze ArrayList.
+ 	 In de loop van spelen wordt over de Arraylist heengegaan met een enhanced for-loop en werp je iedere dobbelsteen.
+ 	 Hou in je dobbelsteen het resultaat van de worp vast.
+x 5. Maak een losse methode vasthouden(). Via een Scanner vraagt het programma om invoer.
+ 	 De invoer wordt via Integer.parseInt() omgezet tot een blokkeerarray van 0 en 1 bijvoorbeeld 11001.
+ 	 Dit blokkeerarray is een field van het YahtzeeSpel.
+x 6. Loop in de tweede worp over de arraylist heen en alleen als in de blokkeerarray een 0 staat wordt de dobbelsteen opnieuw geworpen. 
 
  Nog meer objecten:
  7. Maak een klasse Worp, met een veld van het type int array met lengte 5. Een methode die de worp uitslag weergeeft.
@@ -35,10 +35,11 @@ Meerdere spelers:
 public class Yahtzee {
 	public static void main(String[] args) {
 		boolean doorspelen = true;
-		Scanner invoer = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		
 		while (doorspelen) {
-			System.out.println("1 Spelen\n2 Stoppen");
-			int keuze = invoer.nextInt();				//Veranderen in enter=doorgaan en q=stoppen
+			System.out.println("Welkom bij Yahtzee! Wat wil je doen?\n\n1 Spelen\n2 Stoppen");
+			int keuze = scanner.nextInt();				//Veranderen in enter=doorgaan en q=stoppen
 			if(keuze == 1){
 				YahtzeeSpel spel = new YahtzeeSpel();
 				spel.spelen();
@@ -55,21 +56,56 @@ public class Yahtzee {
 
 class YahtzeeSpel{
 	ArrayList<Dobbelsteen> dobbelstenen = new ArrayList<Dobbelsteen>();
+	int[] blokkeren = {0,0,0,0,0};
 	
 	YahtzeeSpel(){
 		for(int x = 0; x<5; x++) {
 			Dobbelsteen ds = new Dobbelsteen();
-			ds.waarde = ds.werpen();					// Verplaatsen naar methode spelen()?
 			dobbelstenen.add(ds);
 		}
 		System.out.println(dobbelstenen);
 	}
 	
-	public void spelen(){
-		System.out.println("We gaan spelen");
+	void spelen(){
+		System.out.println("\nWe gaan spelen!\n\nWorp 1:");
+		for(int x = 0; x<5;x++) {
+			dobbelstenen.get(x).waarde = dobbelstenen.get(x).werpen();
+		}
+		System.out.println(dobbelstenen);
+		vasthouden();
+		System.out.println("\nWorp 2:");
+		vervolg();
+		vasthouden();
+		System.out.println("\nWorp 3:");
+		vervolg();
 	}
 
+	void vasthouden() {
+		System.out.println("\nWelke dobbelstenen wil je vasthouden?");
+		Scanner sc = new Scanner(System.in);
+		String keuze = sc.nextLine();
+		for(int x = 0; x<keuze.length(); x++) {
+			char c = keuze.charAt(x);
+			String s = Character.toString(c);
+			int y = Integer.parseInt(s)-1;
+			for(int a = 0; a<blokkeren.length; a++) {
+				if(y == a) {
+					blokkeren[a] = 1;
+				}
+			}
+		}
+	}
+	
+	void vervolg() {
+		for(int x = 0; x<5;x++) {
+			if(blokkeren[x] == 0) {
+				dobbelstenen.get(x).waarde = dobbelstenen.get(x).werpen();
+			}	
+		}
+		System.out.println(dobbelstenen);
+	}
 }
+
 class Dobbelsteen{
 	int waarde;
 	
